1a9fbf4fea6b5808bd41de5ab446a8efdbdca525
Glutexo <glutexo@icloud.com>
2017-04-17 09:13:07 +0200

    Vytvoř pahýl vstupního bodu.
    Vytvořen vstupní bod /prezidenti. Samotná adresa /prezidenti
    přesměrovává na /prezidenti/všichni. Tato adresa vypíše jména prezidentů
    z databáze jako seznam. Ten je uložený v SQLite3 databázi, k přístupu se
    používá knihovna Sequel. Na vstupním bodě / je jen pod chybou 400 návod,
    že se má použít jiný.

--- app.rb
+++ app.rb
@@ -1,5 +1,29 @@
 require 'sinatra'
+require 'sequel'
+require 'json'
+
+DB = Sequel.sqlite 'db/database.sqlite'
+
+def json *args
+  JSON.generate *args
+end
+
+before do
+  content_type 'application/json', charset: 'utf-8'
+end
 
 get '/' do
-  'Běží!'
+  halt 400, json('Neplatný vstupní bod. Použij /prezidenti.')
+end
+
+get '/prezidenti' do
+  redirect to '/prezidenti/všichni'
+end
+
+get '/prezidenti/všichni' do
+  names = []
+  DB[:presidents].each do |row|
+    names << row[:name]
+  end
+  json names
 end
---
+++ db/ddl.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `presidents` (
+  `id` INTEGER PRIMARY KEY,
+  `name` VARCHAR
+);
+
+INSERT INTO `presidents`
+  (`name`)
+VALUES
+  ('Tomáš Garrigue Masaryk'),
+  ('Edvard Beneš');
